2fcbcb5f8feeb90d096e46855ec2d15776a3df58
liguangsheng <guangshengli@lilith.com>
2022-12-15 17:10:50 +0800

    add proto3 optional support && filter no need generate

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.16
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/influxdata/influxdb-client-go/v2 v2.4.0 // indirect
-	github.com/lyft/protoc-gen-star v0.5.2
-	golang.org/x/text v0.3.5 // indirect
+	github.com/lyft/protoc-gen-star v0.6.2
+	google.golang.org/protobuf v1.26.0
 )
--- protoc-gen-series/main.go
+++ protoc-gen-series/main.go
@@ -1,7 +1,13 @@
 package main
 
-import pgs "github.com/lyft/protoc-gen-star"
+import (
+	pgs "github.com/lyft/protoc-gen-star"
+	"google.golang.org/protobuf/types/pluginpb"
+)
 
 func main() {
-	pgs.Init(pgs.DebugEnv("DEBUG")).RegisterModule(newSeries()).Render()
+	pbfeatures := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+	pgs.
+		Init(pgs.DebugEnv("DEBUG"), pgs.SupportedFeatures(&pbfeatures)).
+		RegisterModule(newSeries()).Render()
 }
